57e48fbab6691f24e4c564a6926fc8a34b5b5c43
yalcinPXL <yalcinPXL@gmail.com>
2021-07-24 22:44:00 +0200

    Add project files.

---
+++ VirtualizingDemo/Pages/EmployeeListBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VirtualizingDemo.Data;
+using VirtualizingDemo.Models;
+
+namespace VirtualizingDemo.Pages
+{
+    public class EmployeeListBase : ComponentBase
+    {
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+        protected float itemHeight = 50;
+        protected override void OnInitialized()
+        {
+            Employees = EmployeeService.Employees;
+           
+        }
+    }
+}
---
+++ VirtualizingDemo/Pages/EmployeeProviderListBase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VirtualizingDemo.Data;
+using VirtualizingDemo.Models;
+
+namespace VirtualizingDemo.Pages
+{
+    public class EmployeeProviderListBase : ComponentBase
+    {
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+        protected float itemHeight = 50;
+        protected int TotalNumberOfEmployees = 1000;
+
+        protected override void OnInitialized()
+        {
+            Employees = EmployeeService.Employees;
+           
+        }
+        protected async ValueTask<ItemsProviderResult<Employee>> LoadEmployees(ItemsProviderRequest request)
+        {
+            //assume that we have asked the API the total number in a seperate call.
+            var numberOfEmployees = Math.Min(request.Count, TotalNumberOfEmployees - request.StartIndex);
+            var EmployeeListItems = await EmployeeService.GetTakeLongEmployeeList(request.StartIndex, numberOfEmployees);          
+            return new ItemsProviderResult<Employee>(EmployeeListItems, TotalNumberOfEmployees);
+        }
+    }
+}
